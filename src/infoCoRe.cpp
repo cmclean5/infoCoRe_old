210c91356cbe6a37ef4be30c0a50df4aae881620
cmclean5 <Colin.D.Mclean@ed.ac.uk>
2023-01-31 00:02:21 +0000

    changes

--- src/infoCoRe.cpp
+++ src/infoCoRe.cpp
@@ -31,96 +31,35 @@
 
 // [[Rcpp::export]]
 void test( const arma::SpMat<double>& Adj){
-//void test( const arma::Mat<double>& Adj){
-
-  // TEST:
-  // > library(infoCoRe)
-  // > M1<-matrix(rpois(36,5),nrow=6)
-  // > infoCoRe::test(Adj=M1)  
-  //Adj.print();
+  //undirected network
   
-  uint_fast32_t i,N;
+  arma::uword N;
 
   N = Adj.n_rows;
 
-  /*
-  arma::rowvec d_in  = sum(Adj,0);
-  arma::colvec d_out = sum(Adj,1);
-  arma::colvec d_all = d_in + d_out;
-  */
-  arma::SpMat<double> d_in  = sum(Adj,0);
-  arma::SpMat<double> d_out = sum(Adj,1);
-  arma::SpMat<double> d_all = d_in.t() + d_out;
-
-  arma::SpMat<double> D(N,N); D.diag() = d_all;
-
-  d_in.brief_print("d.in:");
-  d_out.brief_print("d.out:");
+  arma::SpMat<double> d = sum(Adj,0);
+  arma::SpMat<double> D(N,N); D.diag() = d;
 
   D.brief_print("D:");
 
   // Laplacian
-  //arma::SpMat<double> L(N,N); L=D-Adj;
   arma::SpMat<double> L(N,N); L=D-Adj;
   
   L.brief_print("L:");
-  //cout << d_all.n_rows << ", " << d_all.n_cols << endl;
-  //d_all.print();
 
-  d_in.transform(  [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
-  d_out.transform( [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
-
-  d_in.brief_print("d.in:");
-  d_out.brief_print("d.out:");
+  D.transform(  [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
 
   // Normalised Laplacian
-  arma::SpMat<double> I; I.eye(N,N);
-  arma::SpMat<double> D_in(N,N);  D_in.diag()  = d_in;
-  arma::SpMat<double> D_out(N,N); D_out.diag() = d_out;
-
-  I.brief_print("I:");
-  D_in.brief_print("D.in:");
-  D_out.brief_print("D.out:");
-  
-  arma::SpMat<double> L_hat = D_in * (Adj+I) * D_out;
+  arma::SpMat<double> L_hat(N,N); L_hat = eye(N,N) - D * Adj * D;
 
   L_hat.brief_print("L.hat:");
-  
-  cout << "\n";
-
-  /*
-  cout << "TEST find indices of non-zero ele in Adj: " << endl;
-
-  arma::uword ii, jj, NR;
-  NR = Adj.n_cols;
-
-  vector<EDGE> edges;
-  
-  for(ii=0; ii<NR; ii++){
-    const arma::SpSubview_col<double> cc = Adj.col(ii);
-    const arma::uvec rindx = find(cc);
-    //p2.brief_print();
-    for(jj=0; jj<rindx.n_elem; jj++){
-      edges.push_back( EDGE(ii,rindx(jj), Adj(ii,rindx(jj))) );
-    }
-  }
-
-  cout << "edges size: " << edges.size() << endl;
-
-  for(i=0; i<20; i++){
-    cout << "[" << std::get<0>(edges[i]) << "," << std::get<1>(edges[i]) << "]" << endl;
-  }
-  
-  //free edges memory vector
-  std::vector<EDGE>().swap(edges);
-  */
- 
   
 }
 
 // [[Rcpp::export]]
 void test2( const arma::SpMat<double>& Adj, Rcpp::IntegerVector weighted=0){
-
+  //Mat<std::complex<double>>
+  
   // Refs: 1) https://stackoverflow.com/questions/67189074/hermitian-adjacency-matrix-of-digraph
   // Refs: 2) https://stackoverflow.com/questions/67189074/hermitian-adjacency-matrix-of-digraph
   // Refs: 3) https://www.stats.ox.ac.uk/~cucuring/Hermitian_Clustering_AISTATS.pdf  
@@ -211,44 +150,21 @@ void test2( const arma::SpMat<double>& Adj, Rcpp::IntegerVector weighted=0){
   //Check H is Hermitain
   cout << "Is H hermitian:  " << H.is_hermitian() << endl;
   cout << "Is H symmetric:  " << H.is_symmetric() << endl;
-  //cout << "Is H her & symm: " << H.is_sympd() << endl;
    
   H.brief_print("H:");
 
-  arma::SpMat<double> d_in  = sum(Adj,0);
-  arma::SpMat<double> d_out = sum(Adj,1);
-  arma::SpMat<double> d_all = d_in.t() + d_out;
-  arma::SpMat<double> D_r(N,N); D_r.diag() = d_all;
-  arma::SpMat<double> D_i(N,N); D_i.zeros();
+  // Laplacian, using in and out-degree
+  arma::SpMat<double> d = sum(Adj,0).t() + sum(Adj,1);
+  arma::SpMat<double> D(N,N); D.diag() = d;
   
-  arma::SpMat<std::complex<double>> L(D_r,D_i);
-
-  L.brief_print("L (before):");
-  
-  // Laplacian
-  L = L-H;
-  //arma::SpMat<std::complex<double>> L(N,N); L=L.diag(D.diag())-H;
-
-  L.brief_print("L (after):");
-  
-  cout << "Is L hermitian:  " << L.is_hermitian() << endl;
-  cout << "Is L symmetric:  " << L.is_symmetric() << endl;
-  //cout << "Is L her & symm: " << L.is_sympd() << endl;
+  arma::SpMat<std::complex<double>> L(N,N); L=D-H;
   
   L.brief_print("L:");
 
-  d_in.transform(  [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
-  d_out.transform( [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
-
-  // Normalised Laplacian  
-  arma::SpMat<double> D_in_r(N,N);  D_in_r.diag()  = d_in;
-  arma::SpMat<double> D_out_r(N,N); D_out_r.diag() = d_out;
-
-  arma::SpMat<std::complex<double>> I; I.eye(N,N);
-  arma::SpMat<std::complex<double>> D_in (D_in_r, D_i);
-  arma::SpMat<std::complex<double>> D_out(D_out_r,D_i);
+  // Normalised Laplacian
+  D.transform(  [](double val) { return ( (val==0 ? val : pow(val,-0.5)) ); });
   
-  arma::SpMat<std::complex<double>> L_hat = D_in * (H+I) * D_out;
+  arma::SpMat<std::complex<double>> L_hat(N,N); L_hat = eye(N,N) - D * H * D;
 
   L_hat.brief_print("L_hat:");
 
@@ -256,5 +172,6 @@ void test2( const arma::SpMat<double>& Adj, Rcpp::IntegerVector weighted=0){
   cout << "Is L_hat symmetric:  " << L_hat.is_symmetric() << endl;
   //cout << "Is L_hat her & symm: " << L_hat.is_sympd() << endl;
 
-  L_hat.brief_print("L_hat:");
+  //return L_hat;
+  
 }
